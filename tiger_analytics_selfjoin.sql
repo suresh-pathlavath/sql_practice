8078590a99ed9a16e5684698e61c39860e9341f9
suresh-pathlavath <sureshpathlavath.iitkgp@gmail.com>
2023-11-24 22:01:35 +0530

    change in format

--- tiger_analytics_selfjoin.sql
+++ tiger_analytics_selfjoin.sql
@@ -1,12 +1,24 @@
-with cte as (SELECT a.cid as cid, a.fid as fid, a.origin as origin, a.destination as destination, 
-b.cid as b_cid, b.fid as b_fid, b.origin as b_origin, b.destination as b_destination
+WITH cte
+     AS (SELECT a.cid         AS cid,
+                a.fid         AS fid,
+                a.origin      AS origin,
+                a.destination AS destination,
+                b.cid         AS b_cid,
+                b.fid         AS b_fid,
+                b.origin      AS b_origin,
+                b.destination AS b_destination
+         FROM   tiger_analytics_flights AS a
+                INNER JOIN tiger_analytics_flights AS b
+                        ON a.destination = b.origin
+                           AND a.cid = b.cid)
+SELECT origin,
+       b_destination AS destination
+FROM   cte
+WHERE  destination IS NOT NULL;
 
- FROM tiger_analytics_flights as a 
-inner JOIN tiger_analytics_flights as b on a.destination = b.origin and a.cid = b.cid)
-SELECT origin, b_destination as destination
- from cte where destination is not null
-;
-SELECT * FROM tiger_analytics_flights;
-SELECT * FROM tiger_analytics_flights;
+SELECT *
+FROM   tiger_analytics_flights;
 
+SELECT *
+FROM   tiger_analytics_flights;
 -- https://www.youtube.com/watch?v=eMQDHHfUJtU&ab_channel=AnkitBansal
