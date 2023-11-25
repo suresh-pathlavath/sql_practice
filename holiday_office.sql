e88bbb5882149f04633472ecb259460537b2aa36
suresh pathlavat <suresh.pathlavat@lockated.com>
2023-11-25 14:31:23 +0530

    changed format and added link

--- holiday_office.sql
+++ holiday_office.sql
@@ -1,14 +1,39 @@
-with cte as (SELECT ticket_id, create_date, resolved_date, 
-timestampdiff(day, create_date, resolved_date) as date_difference,
-timestampdiff(week, create_date, resolved_date) as week_difference,
-timestampdiff(day, create_date, resolved_date) - 2*(timestampdiff(week, create_date, resolved_date)) as excluding_holidays,
-holiday_date, reason,
-count(holiday_date) as cnt
-from date_tickets
-left join date_holidays on holiday_date between create_date and resolved_date
-group by 1,2,3,4,5,6,7,8
-)
-select ticket_id, create_date, resolved_date, excluding_holidays, excluding_holidays - cnt as difference  from cte 
-;
-SELECT * from date_holidays;
-SELECT * from date_tickets;
+--write a query to exclude holidays and show the difference between created_date and resolved date of tickets
+-- https://ik.imagekit.io/suresh29/sql_practice/business_days.pdf
+WITH cte
+     AS (SELECT ticket_id,
+                create_date,
+                resolved_date,
+                Timestampdiff(day, create_date, resolved_date)    AS
+                date_difference,
+                Timestampdiff(week, create_date, resolved_date)   AS
+                week_difference,
+                Timestampdiff(day, create_date, resolved_date) - 2 * (
+                Timestampdiff(week, create_date, resolved_date) ) AS
+                excluding_holidays,
+                holiday_date,
+                reason,
+                Count(holiday_date)                               AS cnt
+         FROM   date_tickets
+                LEFT JOIN date_holidays
+                       ON holiday_date BETWEEN create_date AND resolved_date
+         GROUP  BY 1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7,
+                   8)
+SELECT ticket_id,
+       create_date,
+       resolved_date,
+       excluding_holidays,
+       excluding_holidays - cnt AS difference
+FROM   cte;
+
+SELECT *
+FROM   date_holidays;
+
+SELECT *
+FROM   date_tickets; 
