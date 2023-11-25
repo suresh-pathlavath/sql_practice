db35fb08237eed276a546f0831772b9a08fe436b
suresh pathlavat <suresh.pathlavat@lockated.com>
2023-11-25 11:42:18 +0530

    changed format and added link

--- billing_average.sql
+++ billing_average.sql
@@ -1,10 +1,28 @@
-with cte as (SELECT customer_id, customer_name,
-case when (year(billing_creation_date) =2019) THEN billed_amount else 0 end as customer_2019,
-case when (year(billing_creation_date) =2020) THEN billed_amount else 0 end as customer_2020,
-case when (year(billing_creation_date) =2021) THEN billed_amount else 0 end as customer_2021
-from billing)
-SELECT customer_id, customer_name, avg(customer_2019+ customer_2020+customer_2021) as average from cte
-group by 1,2;
+-- https://ik.imagekit.io/suresh29/sql_practice/billing_amount.pdf
+-- Average billing amount for each customer between 2019 to 2020
+
+WITH cte
+     AS (SELECT customer_id,
+                customer_name,
+                CASE
+                  WHEN ( Year(billing_creation_date) = 2019 ) THEN billed_amount
+                  ELSE 0
+                END AS customer_2019,
+                CASE
+                  WHEN ( Year(billing_creation_date) = 2020 ) THEN billed_amount
+                  ELSE 0
+                END AS customer_2020,
+                CASE
+                  WHEN ( Year(billing_creation_date) = 2021 ) THEN billed_amount
+                  ELSE 0
+                END AS customer_2021
+         FROM   billing)
+SELECT customer_id,
+       customer_name,
+       Avg(customer_2019 + customer_2020 + customer_2021) AS average
+FROM   cte
+GROUP  BY 1,
+          2; 
 SELECT * from billing;
 SELECT (100+150+100)/3
 
