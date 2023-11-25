07bae829a0583826956c67dd77cb6d27de6c33b9
suresh pathlavat <suresh.pathlavat@lockated.com>
2023-11-25 14:56:39 +0530

    changed format and added link

--- marketing_campaign.sql
+++ marketing_campaign.sql
@@ -1,25 +1,33 @@
-
-with cte as (
-
-    select concat(c.first_name, ' ', c.last_name) as customer, camp.name, e.status
-    from marketing_customers as c 
-    left join marketing_campaigns as camp on c.id = camp.customer_id 
-    left join marketing_events as e on camp.id = e.campaign_id 
-)
-, cte2 as (SELECT status as event_type, 
-customer as customer, 
-GROUP_CONCAT(distinct name  SEPARATOR ', ') AS concatenated_values,
-
-count(1) as total ,
-rank() over(partition by status order by count(1) desc) as rnk
-from cte group by 1,2
-order by event_type, total desc
-)
-select event_type, customer, concatenated_values as campign, total from cte2 where rnk = 1;
-
-
-select * from marketing_customers;
-select * from marketing_campaigns;
-select * from marketing_events;
+-- https://ik.imagekit.io/suresh29/sql_practice/marketing_campaign.pdf?updatedAt=1700904345263
+WITH cte AS
+(
+          SELECT    Concat(c.first_name, ' ', c.last_name) AS customer,
+                    camp.NAME,
+                    e.status
+          FROM      marketing_customers AS c
+          LEFT JOIN marketing_campaigns AS camp
+          ON        c.id = camp.customer_id
+          LEFT JOIN marketing_events AS e
+          ON        camp.id = e.campaign_id ) , cte2 AS
+(
+         SELECT   status                                                  AS event_type,
+                  customer                                                AS customer,
+                  group_concat(DISTINCT NAME separator ', ')              AS concatenated_values,
+                  count(1)                                                AS total ,
+                  rank() OVER(partition BY status ORDER BY count(1) DESC) AS rnk
+         FROM     cte
+         GROUP BY 1,
+                  2
+         ORDER BY event_type,
+                  total DESC )
+SELECT event_type,
+       customer,
+       concatenated_values AS campign,
+       total
+FROM   cte2
+WHERE  rnk = 1;SELECT *
+FROM   marketing_customers;SELECT *
+FROM   marketing_campaigns;SELECT *
+FROM   marketing_events;
 
 --  -- https://www.youtube.com/watch?v=W5Wvyc9Pass&ab_channel=techTFQ
