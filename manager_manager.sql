6c444b58988908c07305a6ce9436a996899fe943
suresh pathlavat <suresh.pathlavat@lockated.com>
2023-11-25 14:45:13 +0530

    changed format and added link

--- manager_manager.sql
+++ manager_manager.sql
@@ -1,8 +1,16 @@
-SELECT e.emp_id as emp_id, e.emp_name as emp_name, m.emp_name as manager ,
-s_m.emp_name as senior_manager
-from amazon_employee as e
-left join amazon_employee as m 
-on e.manager_id = m.emp_id
-left join amazon_employee as s_m 
-on m.manager_id = s_m.emp_id ;
-SELECT * from amazon_employee;
+--show employee manager, senior manager with self join concept
+-- https://ik.imagekit.io/suresh29/sql_practice/employee_manager_senior_manager.pdf
+
+SELECT e.emp_id     AS emp_id,
+       e.emp_name   AS emp_name,
+       m.emp_name   AS manager,
+       s_m.emp_name AS senior_manager
+FROM   amazon_employee AS e
+       LEFT JOIN amazon_employee AS m
+              ON e.manager_id = m.emp_id
+       LEFT JOIN amazon_employee AS s_m
+              ON m.manager_id = s_m.emp_id;
+
+SELECT *
+FROM   amazon_employee; 
+
