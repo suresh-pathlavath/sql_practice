8d56bcaf6524dc9be165eeec3f829c5ad973295f
suresh pathlavat <suresh.pathlavat@lockated.com>
2023-11-25 12:41:42 +0530

    changed format and added link

--- employee_summary.sql
+++ employee_summary.sql
@@ -1,19 +1,31 @@
-with logins as (
-    SELECT employeeid, max(timestamp_details) latest_login, count(1) as totallogin from employee_checkin_details where entry_details = 'login' group by 1
-)
-, logouts as (
-    SELECT employeeid, max(timestamp_details) latest_logout, count(1) as totallogin from employee_checkin_details where entry_details = 'logout' group by 1
-)
+-- employee checkin details employeeid, totalentry, totallogin, totallogout, latestlogin, latestlogout
+-- https://ik.imagekit.io/suresh29/sql_practice/employee_checkins.pdf
+ 
+WITH logins
+     AS (SELECT employeeid,
+                Max(timestamp_details) latest_login,
+                Count(1)               AS totallogin
+         FROM   employee_checkin_details
+         WHERE  entry_details = 'login'
+         GROUP  BY 1),
+     logouts
+     AS (SELECT employeeid,
+                Max(timestamp_details) latest_logout,
+                Count(1)               AS totallogin
+         FROM   employee_checkin_details
+         WHERE  entry_details = 'logout'
+         GROUP  BY 1)
+SELECT a.*,
+       b.*
+FROM   logins AS a
+       INNER JOIN logouts AS b
+               ON a.employeeid = b.employeeid;
 
-SELECT a.*, b.*
- from logins as a 
- inner join logouts as b on a.employeeid = b.employeeid
-;
+SELECT *
+FROM   employee_checkin_details;
 
-SELECT * from employee_checkin_details;
-SELECT * from employee_details;
-
-
+SELECT *
+FROM   employee_details;
 /*
 
 https://www.youtube.com/watch?v=u3W_Op3FTVA&ab_channel=AnkitBansal
