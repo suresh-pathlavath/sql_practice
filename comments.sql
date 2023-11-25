095932fc72aecc00d5832be572a1b00940beaa57
suresh pathlavat <suresh.pathlavat@lockated.com>
2023-11-25 12:05:25 +0530

    changed format and added link

--- comments.sql
+++ comments.sql
@@ -1,15 +1,18 @@
-with cte as (SELECT 
-comment, translation,
-case when translation is null then comment 
-    when translation is not null then translation end as expected_output
- from comments_and_translations
- )
- select expected_output from cte 
- ;
- SELECT * from comments_and_translations;
+WITH cte
+     AS (SELECT comment,
+                translation,
+                CASE
+                  WHEN translation IS NULL THEN comment
+                  WHEN translation IS NOT NULL THEN translation
+                END AS expected_output
+         FROM   comments_and_translations)
+SELECT expected_output
+FROM   cte;
 
- /*
+SELECT *
+FROM   comments_and_translations;
+/*
 
- https://www.youtube.com/watch?v=aE623ff7zkM&ab_channel=techTFQ
+https://www.youtube.com/watch?v=aE623ff7zkM&ab_channel=techTFQ
 
- */
+*/
