ed6adbdd473be72a5093c26d11a5cfe508e2358a
suresh pathlavat <suresh.pathlavat@lockated.com>
2023-11-25 14:25:13 +0530

    changed format and added link

--- google_useractivity.sql
+++ google_useractivity.sql
@@ -1,17 +1,31 @@
-with cte as (
+-- second most recent activity of each user
+-- https://ik.imagekit.io/suresh29/sql_practice/google_recent_user_activity.pdf
+WITH cte
+     AS (SELECT username,
+                activity,
+                startdate,
+                enddate,
+                Row_number()
+                  over(
+                    PARTITION BY username
+                    ORDER BY startdate) AS rn,
+                Count(*)
+                  over(
+                    PARTITION BY username
+                    ORDER BY startdate RANGE BETWEEN unbounded preceding AND
+                  unbounded
+                  following
+                  )                     AS cnt_new
+         FROM   google_activity)
+SELECT *
+FROM   cte
+WHERE  rn = CASE
+              WHEN cnt_new = 1 THEN 1
+              ELSE cnt_new - 1
+            END;
 
-    select username, activity, startdate, enddate, 
-    row_number() over(partition by username order by startdate) as rn,
-    count(*) over(partition by username order by startdate range between unbounded PRECEDING and UNBOUNDED FOLLOWING) as cnt_new 
-    
-    from google_activity
-)
-select *
-  from cte 
-  where rn = case when cnt_new = 1 then 1 else cnt_new-1  end ;
-
-select * from google_activity; 
-
+SELECT *
+FROM   google_activity;
 /*
 
 https://www.youtube.com/watch?v=ueOUSjdAZY8&ab_channel=techTFQ
