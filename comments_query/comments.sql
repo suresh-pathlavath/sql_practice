47f137dac64c438d9a69da433e3030745dce1638
suresh pathlavat <suresh.pathlavat@lockated.com>
2023-11-29 17:09:45 +0530

    query to folder

---
+++ comments_query/comments.sql
@@ -0,0 +1,20 @@
+-- remove Garbage values and show meaningful comments
+-- https://ik.imagekit.io/suresh29/sql_practice/meaningful_comments.pdf
+WITH cte
+     AS (SELECT comment,
+                translation,
+                CASE
+                  WHEN translation IS NULL THEN comment
+                  WHEN translation IS NOT NULL THEN translation
+                END AS expected_output
+         FROM   comments_and_translations)
+SELECT expected_output
+FROM   cte;
+
+SELECT *
+FROM   comments_and_translations;
+/*
+
+https://www.youtube.com/watch?v=aE623ff7zkM&ab_channel=techTFQ
+
+*/
