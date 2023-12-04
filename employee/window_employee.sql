c03d2dfdda4db6e76a54da814c17cd6fe56cb482
suresh-pathlavath <sureshpathlavath.iitkgp@gmail.com>
2023-12-04 11:35:48 +0530

    few changes

---
+++ employee/window_employee.sql
@@ -0,0 +1,18 @@
+SELECT emp_name,
+    salary,
+    rank() OVER(
+        ORDER BY salary DESC
+    ) as rnk,
+    DENSE_RANK() OVER(
+        ORDER BY salary DESC
+    ) dense_rnk,
+    ROW_NUMBER() OVER(
+        ORDER BY salary DESC
+    ) as row_n
+FROM employee;
+SELECT * FROM employee;
+
+    /*
+     https://www.youtube.com/watch?v=xMWEVFC4FOk&ab_channel=AnkitBansal
+     */
+
