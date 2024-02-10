1727c34958bbcd6a686769aa768b5a9f23f69abc
suresh <sureshpathlavath.iitkgp@gmail.com>
2024-02-11 00:20:50 +0530

    few changes

--- clock_based/clock_based.sql
+++ clock_based/clock_based.sql
@@ -1,41 +1,13 @@
 /*
-
-******************************  Table Creation & Problem Statement  ***************************
-Problem Statement: find the total number of hours clocked by each employee in the office 
+-- Problem Statement: find the total number of hours clocked by each employee in the office 
 I: means punch in 
 O: means punch out
 Note: Employee can do multiple punch in and punch out
-
-create table clocked_hours(
-
-empd_id int,
-
-swipe time,
-
-flag char
-
-)
-
-insert into clocked_hours values
-
-(11114,'08:30','I'),
-
-(11114,'10:30','O'),
-
-(11114,'11:30','I'),
-
-(11114,'15:30','O'),
-
-(11115,'09:30','I'),
-
-(11115,'17:30','O');
-
-
 */
 
+
 --  First Method
-SELECT *
-FROM   clocked_hours;
+SELECT * FROM   clocked_hours;
 
 WITH cte
      AS (SELECT *,ROW_NUMBER() OVER( partition BY empd_id, flag ORDER BY swipe) AS rn
@@ -63,3 +35,33 @@ SELECT empd_id,
 FROM   cte
 WHERE  flag = 'I'
 GROUP  BY empd_id; 
+
+
+/*
+
+create table clocked_hours(
+
+empd_id int,
+
+swipe time,
+
+flag char
+
+);
+
+insert into clocked_hours values
+
+(11114,'08:30','I'),
+
+(11114,'10:30','O'),
+
+(11114,'11:30','I'),
+
+(11114,'15:30','O'),
+
+(11115,'09:30','I'),
+
+(11115,'17:30','O');
+
+
+*/
