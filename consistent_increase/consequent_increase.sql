4e2d53b5b0a050677ceb1e81b4faf43254a5364d
suresh <sureshpathlavath.iitkgp@gmail.com>
2024-10-30 15:43:03 +0530

    formatted code for better readibility

--- consistent_increase/consequent_increase.sql
+++ consistent_increase/consequent_increase.sql
@@ -1,37 +1,13 @@
--- identify cities where covid cases increasing everyday(consistently)
--- https://ik.imagekit.io/suresh29/sql_practice/everyday_increasing_cases.pdf
-WITH cte
-     AS (SELECT city,
-                days,
-                cases,
-                Dense_rank()
-                  over(
-                    PARTITION BY city
-                    ORDER BY days)
-                   AS rnk_days,
-                Dense_rank()
-                  over(
-                    PARTITION BY city
-                    ORDER BY cases)
-                   AS rnk_cases,
-                Convert(Dense_rank()
-                          over(
-                            PARTITION BY city
-                            ORDER BY days), DECIMAL) -
-                Convert(Dense_rank()
-                          over(
-                            PARTITION BY city
-                            ORDER BY cases), DECIMAL) AS
-                diff
-         FROM   covid
-         ORDER  BY city,
-                   days)
+WITH cte AS (
+    SELECT city, days, cases,
+        DENSE_RANK() OVER (PARTITION BY city ORDER BY days) AS rnk_days,
+        DENSE_RANK() OVER (PARTITION BY city ORDER BY cases) AS rnk_cases,
+        DENSE_RANK() OVER (PARTITION BY city ORDER BY days) - DENSE_RANK() OVER (PARTITION BY city ORDER BY cases) AS diff
+    FROM covid
+)
 SELECT city
-FROM   cte
-GROUP  BY city
-HAVING Count(DISTINCT diff) = 1;
+FROM cte
+GROUP BY city
+HAVING COUNT(DISTINCT diff) = 1;
 
-SELECT *
-FROM   covid; 
-
-
+SELECT * FROM covid;
